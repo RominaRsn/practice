92a8ce175ebe5246f64c0150c5003a9bd15cff50
RominaRsn <62810651+RominaRsn@users.noreply.github.com>
2020-04-06 10:51:41 -0700

    Add files via upload
    practicing format

---
+++ mad_libs.py
@@ -0,0 +1,9 @@
+
+v=[]
+print("input the name of your hero:")
+v.append(input())
+print("input the age of your hero:")
+v.append(input())
+print("input your hero's eye color")
+v.append(input())
+print("this story is about a hero called {} he/she is {} and his/her eyes are {}".format(v[0],v[1],v[2]))
